3f0c239b67983ec648822f26a06bfc2919aec5ec
TREJO <trejojfrancisco@gmail.com>
2023-03-30 13:57:14 +0200

    primera carga

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Ejercicio3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Hacer un programa para ingresar un número 
+            // y luego se emita por pantalla un cartel aclaratorio si “Es mayor a 10” o “No es mayor a 10”.
+
+            int n1;
+
+            Console.WriteLine("Ingrese un número ");
+            n1 = int.Parse(Console.ReadLine());
+
+            if (n1 > 10)
+                Console.WriteLine("Es mayor a 10");
+            else
+                Console.WriteLine("No es mayor a 10");
+            
+            Console.WriteLine("Fin del programa");
+        }
+    }
+}
